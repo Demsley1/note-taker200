44de342e0154e781d1da325bbc35adfeaf156169
Denva <emsley3684@gmail.com>
2021-12-15 22:53:28 -0500

    express middleware and intial get endpoint

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+// Express middleware to handle json data resposnes, and files run on server , port call
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Intiial app response
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
