c782620e4a186f1b025192e3663a60c5cf9aa4f8
Denva <emsley3684@gmail.com>
2021-12-17 13:36:02 -0500

    added new file to handle not save, get, and delete back-end functions

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
-const dataBase = require('../../db/db.json')
+const { createNewNote } = require('../../lib/note');
+const dataBase = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    
+});
 
 router.post('/notes', (req, res) => {
     req.body.id = dataBase.length.toString();
-    
-})
+    const note = createNewNote(req.body, dataBase);
+    res.json(note);
+});
 
 module.exports = router;
