8548779e7a55ffa6852a9b5a26638639566dd9be
Denva <emsley3684@gmail.com>
2021-12-17 15:29:55 -0500

    consolidated express path function, and server-side save note function

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,16 +1,29 @@
 const router = require('express').Router();
-const { createNewNote } = require('../../lib/note');
-const { database } = require('../../db/db.json');
+/*const { createNewNote } = require('../../lib/note');*/
+const database = require('../../db/db.json');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/notes', (req, res) => {
     let results = database
     res.json(results);
 });
 
-/*router.post('/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     req.body.id = database.length.toString();
+    
+    const createNewNote = (data, dbArray) => {
+        const database = dbArray
+        database.push(data);
+        fs.writeFileSync(
+            path.join(__dirname, '../../db/db.json'),
+            JSON.stringify( dbArray, null, 2)
+        );
+        return database;
+    };
+
     const note = createNewNote(req.body, database);
     res.json(note);
-});*/
+});
 
 module.exports = router;
